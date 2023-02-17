baf6065926b7c5355f15f5a0a5fe404861cb0b4b
romeugomes <romeugomes88@gmail.com>
2023-02-16 23:31:52 -0300

    configuração as classes de dominio

--- src/main/java/com/romeu/bookstore/domain/Categoria.java
+++ src/main/java/com/romeu/bookstore/domain/Categoria.java
@@ -1,16 +1,31 @@
 package com.romeu.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
-			
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id, nome);
@@ -32,15 +47,13 @@ public class Categoria {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Categoria(Integer id, String nome, String descricao) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
 	}
-
-
 
 	public Integer getId() {
 		return id;
