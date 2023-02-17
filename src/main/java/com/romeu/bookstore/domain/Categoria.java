6ab6f9a85c062588b7e7fa305e3c66ea34806c7b
romeugomes <romeugomes88@gmail.com>
2023-02-16 23:37:23 -0300

    configuração as classes de dominio

--- src/main/java/com/romeu/bookstore/domain/Categoria.java
+++ src/main/java/com/romeu/bookstore/domain/Categoria.java
@@ -23,7 +23,7 @@ public class Categoria implements Serializable {
 	private String nome;
 	private String descricao;
 
-	@OneToMany(mappedBy = "categoria" )
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	@Override
