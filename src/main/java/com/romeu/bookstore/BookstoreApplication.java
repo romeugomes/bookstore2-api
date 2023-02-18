9d2d62b7382cd1ae4ebc78eddf1dd3e6e6ed3b5c
romeugomes <romeugomes88@gmail.com>
2023-02-18 11:22:05 -0300

    Criada classe de config e de serviço

--- src/main/java/com/romeu/bookstore/BookstoreApplication.java
+++ src/main/java/com/romeu/bookstore/BookstoreApplication.java
@@ -1,41 +1,13 @@
 package com.romeu.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.romeu.bookstore.domain.Categoria;
-import com.romeu.bookstore.domain.Livro;
-import com.romeu.bookstore.repositories.CategoriaRepository;
-import com.romeu.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-
-	}
 }
