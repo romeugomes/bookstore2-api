aabe2fe55793ef373bf456596ec057c63ce4572d
romeugomes <romeugomes88@gmail.com>
2023-02-18 10:12:55 -0300

    Ajustar as classe de dominio com a referencia da entidade

--- src/main/java/com/romeu/bookstore/domain/Categoria.java
+++ src/main/java/com/romeu/bookstore/domain/Categoria.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 @Entity
--- src/main/java/com/romeu/bookstore/domain/Livro.java
+++ src/main/java/com/romeu/bookstore/domain/Livro.java
@@ -3,11 +3,10 @@ package com.romeu.bookstore.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
